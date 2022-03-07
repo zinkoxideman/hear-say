bc47692b30b592d2133ecd0569e7410ad141aa84
Walenberg <g.a.m.t.walenberg@student.tue.nl>
2022-03-07 13:09:56 +0100

    Removed unnececary parts of the interpreter

--- SERVER/process.py
+++ SERVER/process.py
@@ -1,28 +1,12 @@
-import os
-import pathlib
-
-import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 import tensorflow as tf
+import numpy as np
 
-from tensorflow.keras import layers
-from tensorflow.keras import models
-from IPython import display
-
-import matplotlib.pyplot as plot 
-import random 
-import numpy as np 
 emotions = ['angry', 'anxious', 'apologetic', 'assertive', 'concerned', 'encouraging', 'excited', 'happy', 'neutral', 'sad']
-
-def decode_audio(audioData):    
-    return 
-
 
 def get_spectrogram(waveform):
     # # Convert the waveform to a spectrogram via a STFT.
-    spectrogram = tf.signal.stft(
-        waveform, frame_length=255, frame_step=128)
+    spectrogram = tf.signal.stft(waveform, frame_length=255, frame_step=128)
     # Obtain the magnitude of the STFT.
     spectrogram = tf.abs(spectrogram)
     # Add a `channels` dimension, so that the spectrogram can be used
@@ -42,10 +26,11 @@ def get_spectrogram(waveform):
 def interpretAudio(audioData):
     print("hoi")
 
-    # model = tf.keras.models.load_model('model')
+    model = tf.keras.models.load_model('C:/Users/20193530/OneDrive - TU Eindhoven/Desktop/New folder (2)/New folder/hear-say/SERVER/model/')
+
     # print(model)
     print(get_spectrogram(tf.squeeze(audioData, axis=-1)))
-    # prediction = model(get_spectrogram(tf.squeeze(audioData, axis=-1)))
-    # return tf.nn.softmax(prediction[0])
+    prediction = model(get_spectrogram(tf.squeeze(audioData, axis=-1)))
+    return tf.nn.softmax(prediction[0])
 
 interpretAudio(np.random.rand(44100, 1))
